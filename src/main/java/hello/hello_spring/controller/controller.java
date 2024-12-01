d2c4a990de40d51aa621ea64c43c26268fcfdff4
gwgtis135 <gwgtis135@naver.com>
2024-12-01 22:15:38 +0900

    Initial commit

---
+++ src/main/java/hello/hello_spring/controller/controller.java
@@ -0,0 +1,50 @@
+package hello.hello_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class controller {
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "안녕!!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody   //응답 body부에 리턴을 직접 넣어주겠다는 의미   //리턴이 문자 그대로가 감
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;   // 객체를 넘김  {"name":"123123123"} 이렇게 출력됨 json
+                        // 객체를 리턴하면 json타입으로 돌려주겠다는 건 약속임
+    }
+
+    //Hello라는 객체 만들기 static로 하나의 클래스 안에서 사용
+    static class Hello{
+        private String name;
+
+        public String getName(){
+            return name;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
+    }
+}
